a487333c37aa9b1cb892de6fafddce18f6fbc336
manglemix <38180553+manglemix@users.noreply.github.com>
2020-06-09 12:33:12 +0800

    Optimized motion_blur
    motion_blur can now directly access the linear velocity from the StateMachine. Unfortunately the tool keyword had to be removed because it was causing errors as the StateMachine does not run in the editor

--- motion_blur/motion_blur.gd
+++ motion_blur/motion_blur.gd
@@ -1,17 +1,19 @@
-tool
 extends MeshInstance
 
-var cam_pos_prev = Vector3()
-var cam_rot_prev = Quat()
+
+export var _camera_controller_path: NodePath
+export var blur_factor := 1.5
+
+var cam_rot_prev: Quat
+
+onready var camera_controller := get_node(_camera_controller_path)
+
 
 func _process(delta):
 	
 	var mat = get_surface_material(0)
 	var cam = get_parent()
 	assert(cam is Camera)
-	
-	# Linear velocity is just difference in positions between two frames.
-	var velocity = cam.global_transform.origin - cam_pos_prev
 	
 	# Angular velocity is a little more complicated, as you can see.
 	# See https://math.stackexchange.com/questions/160908/how-to-get-angular-velocity-from-difference-orientation-quaternion-and-time
@@ -21,12 +23,13 @@ func _process(delta):
 	var ang_vel = (cam_rot_diff * 2.0) * cam_rot_conj; 
 	ang_vel = Vector3(ang_vel.x, ang_vel.y, ang_vel.z) # Convert Quat to Vector3
 	
-	mat.set_shader_param("linear_velocity", velocity)
-	mat.set_shader_param("angular_velocity", ang_vel)
+	print(camera_controller.linear_velocity * delta * blur_factor)
+	mat.set_shader_param("linear_velocity", camera_controller.linear_velocity * delta * blur_factor)
+	mat.set_shader_param("angular_velocity", ang_vel * blur_factor)
 		
-	cam_pos_prev = cam.global_transform.origin
 	cam_rot_prev = Quat(cam.global_transform.basis)
-	
+
+
 # Calculate the conjugate of a quaternion.
 func conjugate(quat):
 	return Quat(-quat.x, -quat.y, -quat.z, quat.w)
