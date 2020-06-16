9c57b4d7384aa9b91b6f34ec76db125954e82221
manglemix <38180553+manglemix@users.noreply.github.com>
2020-06-16 13:49:29 +0800

    Added dragging to camera
    While pressing the middle mouse button, the player can move the camera

--- scenes/standard_camera/camera_move.gd
+++ scenes/standard_camera/camera_move.gd
@@ -10,8 +10,11 @@ export var min_z := 20.0
 export var max_z := 300.0
 export var zoom_step := 20.0
 
+export var drag_sensitivity := 0.5
+
 var target_z: float
-var dont_save := ["player"]
+var dont_save := ["player", "dragging"]
+var last_mouse_position: Vector2
 
 onready var player: KinematicPlus = get_parent().get_parent()
 
@@ -21,22 +24,31 @@ func enter(msg := {}) -> void:
 
 
 func _process(delta):
-	var direction := Vector3(
-			Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-			Input.get_action_strength("move_up") - Input.get_action_strength("move_down"),
-			0
-		)
-	
-	if Input.is_action_pressed("move_fast"):
-		direction *= fast_speed
-	
-	elif Input.is_action_pressed("move_slow"):
-		direction *= slow_speed
+	if Input.is_action_pressed("drag_camera"):
+		var tmp: Vector2 = get_viewport().get_mouse_position()
+		var tmp_velocity: Vector2 = (last_mouse_position - tmp) / delta * drag_sensitivity
+		player.linear_velocity.x = tmp_velocity.x
+		player.linear_velocity.y = - tmp_velocity.y
+		last_mouse_position = tmp
 	
 	else:
-		direction *= normal_speed
+		var direction := Vector3(
+				Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+				Input.get_action_strength("move_up") - Input.get_action_strength("move_down"),
+				0
+			)
 		
-	player.linear_velocity = player.linear_velocity.linear_interpolate(direction, move_weight * delta)
+		if Input.is_action_pressed("move_fast"):
+			direction *= fast_speed
+		
+		elif Input.is_action_pressed("move_slow"):
+			direction *= slow_speed
+		
+		else:
+			direction *= normal_speed
+			
+		player.linear_velocity = player.linear_velocity.linear_interpolate(direction, move_weight * delta)
+	
 	player.global_transform.origin.z = lerp(player.global_transform.origin.z, target_z, move_weight * delta)
 
 
@@ -52,3 +64,6 @@ func _input(event):
 		
 		if target_z > max_z:
 			target_z = max_z
+	
+	elif event.is_action_pressed("drag_camera"):
+		last_mouse_position = get_viewport().get_mouse_position()
