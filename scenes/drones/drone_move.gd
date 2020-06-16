3e615452b43934a289b44cf373e5e077428273e1
manglemix <38180553+manglemix@users.noreply.github.com>
2020-06-16 13:49:57 +0800

    Drone can now move to a node
    This will mostly be used on waypoints

--- scenes/drones/drone_move.gd
+++ scenes/drones/drone_move.gd
@@ -6,28 +6,40 @@ export var acceleration := 25.0
 export var turn_weight := 6.0
 export var max_distance := 2.0
 
+var dont_save := ["target_node"]
 var speed: float
 var target: Vector3
+var target_node: Spatial
 
 onready var drone: KinematicPlus = get_parent().get_parent()
 
 
 func _ready():
-	move_to(Vector3(100, 100, 0))
+	move_to_node(get_node("../../../Atlas"))
 
 
 func move_to(position: Vector3) -> void:
 	target = position
+	target_node = null
+	set_physics_process(true)
+
+
+func move_to_node(node: Spatial) -> void:
+	target_node = node
 	set_physics_process(true)
 
 
 func _physics_process(delta):
+	if target_node != null:
+		target = target_node.global_transform.origin
+	
 	var distance := drone.global_transform.origin.distance_to(target)
 	var target_speed := clamp(sqrt(2 * acceleration * distance), 0, max_speed)
 	
 	if distance < max_distance:
 		speed = 0
 		set_physics_process(false)
+		target_node = null
 	
 	else:
 		if speed < target_speed:
@@ -37,5 +49,3 @@ func _physics_process(delta):
 	
 	drone.global_transform = drone.global_transform.interpolate_with(drone.global_transform.looking_at(target, Vector3.BACK), turn_weight * delta)
 	drone.linear_velocity = - speed * drone.global_transform.basis.z
-	
-
